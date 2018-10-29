abcf50cbb885f20b218279fc4799fc3e5d06b73f
Romain FENNINGER <romain@fenninger.fr>
2018-10-29 18:41:49 +0100

    Debug ESP OTA

--- lib/OTA/src/OTA.cpp
+++ lib/OTA/src/OTA.cpp
@@ -4,7 +4,10 @@
 
 int _port;
 
-void OTA::init(int port) {
+const char* serverIndex = "<form method='POST' action='/update' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Update'></form>";
+
+
+void IRAM_ATTR OTA::init(int port) {
   Serial.println("Init OTA server");
   _port = port;
   // Serial.println(_port);
@@ -14,7 +17,7 @@ void OTA::init(int port) {
   _WebServer->on("/", HTTP_GET, [&](){
     Serial.println('HTTP GET');
     _WebServer->sendHeader("Connection", "close");
-    _WebServer->send(200, "text/html", "HELLO");
+    _WebServer->send(200, "text/html", "TUTU");
   });
 
   _WebServer->on("/update", HTTP_POST, [&](){
@@ -29,17 +32,20 @@ void OTA::init(int port) {
       // WiFiUDP::stopAll();
       Serial.printf("Update: %s\n", upload.filename.c_str());
       // uint32_t maxSketchSpace = (ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000;
-      if(!Update.begin(0xFFFFFFFF)){//start with max available size
+      if(!Update.begin()){
+        Serial.println("Error after begin");
         Update.printError(Serial);
       }
     } else if(upload.status == UPLOAD_FILE_WRITE){
       if(Update.write(upload.buf, upload.currentSize) != upload.currentSize){
+        // Serial.println("Error after write");
         Update.printError(Serial);
       }
     } else if(upload.status == UPLOAD_FILE_END){
       if(Update.end(true)){ //true to set the size to the current progress
         Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
       } else {
+        Serial.println("Error Unknonw");
         Update.printError(Serial);
       }
       Serial.setDebugOutput(false);
